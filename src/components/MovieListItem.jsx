3c00ecc0b4baa982b909901743d60278f0b6d616
Dakshraj Sharma <sharmarajdaksh@gmail.com>
2020-05-01 18:26:37 +0530

    Removed unnecessary code

--- src/components/MovieListItem.jsx
+++ src/components/MovieListItem.jsx
@@ -1,28 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef } from 'react';
 
 const MovieListItem = ({ movie }) => {
-	const imgRef = useRef(null);
-	const [imgCheck, setImgCheck] = useState(false);
-
-	useEffect(() => {
-		if (
-			imgRef.current &&
-			imgRef.current.complete &&
-			imgRef.current.naturalHeight !== 0
-		) {
-			setImgCheck(true);
-		}
-	}, [imgRef]);
-
-	useEffect(() => {
-		console.log('load');
-		setImgCheck(true);
-	}, [imgCheck]);
-
-	return imgCheck ? (
+	return (
 		<div key={movie.imdbID} className='MovieListItem'>
 			<div className='MovieListItem-poster'>
-				<img ref={imgRef} src={movie.Poster} alt={movie.Title} />
+				<img src={movie.Poster} alt={movie.Title} />
 			</div>
 			<div className='MovieListItem-details-aside'>
 				<p className='MovieListItem-details-aside-title'>
@@ -31,7 +13,7 @@ const MovieListItem = ({ movie }) => {
 				<p className='MovieListItem-details-aside-year'>{movie.Year}</p>
 			</div>
 		</div>
-	) : null;
+	);
 };
 
 export default MovieListItem;
